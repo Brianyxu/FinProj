e79953a26aa4fcb13d77d79719aaeaee4ad4f09e
Brianyxu <brian.y.xu@vanderbilt.edu>
2019-02-23 09:15:50 -0600

    initial commit

--- js/index.js
+++
@@ -1,6 +0,0 @@
-var form = document.getElementById("form-id");
-
-
-document.getElementById("buttonid").addEventListener("click", function () {
-  document.getElementById("demo").innerHTML = "Hello World";
-});
