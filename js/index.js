b6f87197b0149eb5df1a54d30e2a041c9a02a457
Brianyxu <brian.y.xu@vanderbilt.edu>
2019-02-23 05:13:00 -0600

    First commit, please help me Felix

---
+++ js/index.js
@@ -0,0 +1,6 @@
+var form = document.getElementById("form-id");
+
+
+document.getElementById("buttonid").addEventListener("click", function () {
+  document.getElementById("demo").innerHTML = "Hello World";
+});
