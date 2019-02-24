df192b8dcf74644817cd65cb9e45db17193fbae1
Brianyxu <brian.y.xu@vanderbilt.edu>
2019-02-24 01:26:43 -0600

    hm

--- hello.py
+++ hello.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 from portfolize import *
 import math
-import html
 app = Flask(__name__)
 
 @app.route('/')
@@ -15,16 +14,15 @@ def success():
         budget = int(result['Budget'])
         goal = int(result['goal'])
         risk = result['risk']
-        label, value = run(budget, goal, risk=='high risk')
+        label, value, roi = run(budget, goal, risk=='high risk')
         value = value * budget
-        for i in label:
-            i=html.unescape(i)
         for i in range(len(value)):
             value[i]='%.02f' % (value[i] * budget)
         colors=[]
+        roi = '%.02f' % roi
         for i in range(len(value)):
             colors.append('#%06X' % int(16777215/math.sqrt(i+1)))
-        return render_template("success.html", budget = budget, goal = goal, risk = risk, set=zip(value,label, colors))
+        return render_template("success.html", budget = budget, goal = goal, risk = risk, roi = roi, set=zip(value,label, colors))
 
 @app.route('/about')
 def about():
@@ -34,5 +32,8 @@ def helper(a):
     for item in a:
         formatted_list.append("%.2f"%item)
     return (formatted_list)
+
+
 if __name__ == '__main__':
-    app.run(debug=True) 
+    port = int(os.environ.get('PORT', 33507))
+    app.run(host='0.0.0.0', port=port)
